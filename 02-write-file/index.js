c4c2971cd55473717f6a8c58e62c8c838021cd89
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-11-01 15:25:10 +0300

    feat: add solution for 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFile(
+  path.join(__dirname, 'task2.txt'),
+  '',
+  (err) => {
+    if (err) throw err;
+    // console.log('Файл был создан');
+  }
+);
+
+const { stdin, stdout, stderr } = process;
+stdout.write('Пожалуйста введите любой текст \n');
+stdin.on('data', data => {
+  if (data.toString() == '.exit\r\n') {
+    stdout.write('Bye-bye');
+    process.exit();
+  }//обрабатывает выход через .exit+enter
+  fs.appendFile(
+    path.join(__dirname, 'task2.txt'),
+    `${data}`,
+    err => {
+      if (err) throw err;
+      // console.log('Файл был изменен');
+    }
+  );
+});
+
+process.on('SIGINT', () => {
+  stdout.write('Bye-bye');
+  process.exit();
+});//обрабатывает выход через ctrl+c
