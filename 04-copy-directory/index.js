64020d50523a5f6fc30cf7a0958f9e147154e52b
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-11-03 14:57:36 +0300

    feat: add solution for 03-files-in-folder task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const { copyFile, readdir } = require('node:fs/promises');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+  if (err) throw err;//если папка уже существует, то выдает ошибку
+  console.log('Папка была создана');
+})
