84509b0fb61432dc1cbbe4239ccf6c23b7bff687
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-11-03 15:16:17 +0300

    refactor: re-write fs.stats as async function w/o callback

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,18 @@
 const path = require('path');
-const { readdir } = require('fs/promises');
+const { readdir, stat } = require('fs/promises');
 const fs = require('fs');
 
 async function readFilesInfo() {
   try {
     const files = await readdir(path.join(__dirname, 'secret-folder'));
     for (const file of files) {
-      fs.stat(path.join(__dirname, 'secret-folder', `${file}`), (err, stats) => {
-        if (err) {
-          console.error(err)
-          return
+      const stats = await stat (path.join(__dirname, 'secret-folder', `${file}`));
+      if (stats.isFile()) {
+        let name = path.basename(file).split('.')[0];
+        let ext = path.extname(file).slice(1);
+        let size = stats.size;
+        console.log(`${name} - ${ext} - ${size}b`);
         }
-        if (stats.isFile()) {
-          let name = path.basename(file).split('.')[0];
-          let ext = path.extname(file).slice(1);
-          let size = stats.size;
-          console.log(`${name} - ${ext} - ${size}b`);
-          }
-        });
       };
   } catch (err) {
     console.error(err);
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,28 @@ const path = require('path');
 const { copyFile, readdir } = require('node:fs/promises');
 
 fs.mkdir(path.join(__dirname, 'files-copy'), err => {
-  if (err) throw err;//если папка уже существует, то выдает ошибку
+  if (err) {
+    // throw err;//если папка уже существует, то выдает ошибку
+    fs.rmdir(path.join(__dirname, 'files-copy'), err => {
+      if(err) throw err; // не удалось удалить папку
+      console.log('Папка успешно удалена');
+    });
+  }
   console.log('Папка была создана');
-})
+});
+
+async function readFiles() {
+  try {
+    const files = await readdir(path.join(__dirname, 'files'));
+    console.log(files);
+    return files;
+    // for (const file of files) {
+
+    // }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// readFiles();
+console.log(readFiles());
