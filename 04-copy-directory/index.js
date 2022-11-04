6480e3e0f85d1d80ae097c4f5a043effb34e591f
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-11-04 16:36:51 +0300

    feat: add solution for 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,54 @@
 const fs = require('fs');
 const path = require('path');
-const { copyFile, readdir } = require('node:fs/promises');
+const { copyFile, readdir, stat } = require('node:fs/promises');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), err => {
-  if (err) {
-    // throw err;//если папка уже существует, то выдает ошибку
-    fs.rmdir(path.join(__dirname, 'files-copy'), err => {
-      if(err) throw err; // не удалось удалить папку
-      console.log('Папка успешно удалена');
-    });
-  }
-  console.log('Папка была создана');
-});
+async function addFolder() {
+  await fs.promises.mkdir(path.join(__dirname, 'files-copy'));//,
+  // (err) => console.error(err));
+  // console.log('Папка была создана');
+}
 
-async function readFiles() {
+async function deleteFolder() {
+  await fs.promises.rm(path.join(__dirname,'files-copy'), { recursive:true, force: true });//,
+  // (err) => console.error('err', err));
+  // console.log('Папка успешно удалена');
+}
+
+async function hasCopyDir() {
   try {
-    const files = await readdir(path.join(__dirname, 'files'));
-    console.log(files);
-    return files;
-    // for (const file of files) {
+    const folders = await readdir(path.dirname(__filename));
+    // console.log('folders', folders);
+    if (folders.includes('files-copy')) {
+      // console.log('files-copy существует - удаляем - проверяем заново');
+      await deleteFolder();
+      hasCopyDir();
+    } else {
+      // console.log('files-copy не существует - создаем');
+      // addFolder();
+      copyFiles();
+    }
+  } catch (err) {
+    console.error('error', err);
+  }
+}
 
-    // }
+async function copyFiles() {
+  try {
+    addFolder();
+    const files = await readdir(path.join(__dirname, 'files'));
+    // console.log('files', files);
+
+    for (const file of files) {
+      const copy = await copyFile(path.join(__dirname, 'files', `${file}`), path.join(__dirname, 'files-copy', `${file}`));
+      // console.log('copied successfully');
+    }
+    console.log('copied successfully')
   } catch (err) {
     console.error(err);
   }
 }
 
-// readFiles();
-console.log(readFiles());
+// deleteFolder();
+// addFolder();
+hasCopyDir();
+// copyFiles();
