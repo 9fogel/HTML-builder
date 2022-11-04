3ac54ac54acc07fe44cf809e8ce2a7cd83f1f057
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-11-04 16:56:40 +0300

    feat: add solution for 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir, stat } = require('node:fs/promises');
+
+fs.writeFile(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+async function mergeStyles() {
+  const files = await readdir(path.join(__dirname, 'styles'));
+  // console.log('files', files);
+
+  for (const file of files) {
+    let ext = path.extname(file).slice(1);
+    const stats = await stat (path.join(__dirname, 'styles', `${file}`));
+
+    if (stats.isFile() && ext === 'css') {
+      const readableStream = fs.createReadStream(path.join(__dirname, 'styles', `${file}`));
+      readableStream.on('data', data => {
+        fs.appendFile(
+          path.join(__dirname, 'project-dist', 'bundle.css'),
+          `${data}`,
+          err => {
+            if (err) throw err;
+          }
+        );
+    });
+    }
+  }
+  console.log('merge completed successfully');
+}
+
+mergeStyles();
